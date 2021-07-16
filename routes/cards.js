56295e754f8c50ec83b903e509e494f8fae1e591
anwarmontasir <anwarmontasir@gmail.com>
2021-07-16 15:54:33 -0700

    create random question route

--- routes/cards.js
+++ routes/cards.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+    const numberOfQuestions = cards.length;
+    const questionNumber = Math.floor(Math.random() * numberOfQuestions);
+    res.redirect(`/cards/${questionNumber}?side=question`);
+});
+
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
